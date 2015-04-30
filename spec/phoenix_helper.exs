b73c5bdd4ea5c1b184505e84a7c1b7c21d842efe
Anton Mishchuk <anton.mishchuk@onapp.com>
2015-04-30 18:04:29 +0300

    0.1.0

--- spec/phoenix_helper.exs
+++
@@ -1,5 +0,0 @@
-Code.require_file("spec/espec_phoenix_extend.ex")
-
-Mix.Task.run "ecto.create", ["--quiet"]
-Mix.Task.run "ecto.migrate", ["--quiet"]
-Ecto.Adapters.SQL.begin_test_transaction(App.Repo)
