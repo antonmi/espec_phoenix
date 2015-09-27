d6db09e43b7ff5e63cf901484485ce5df57b43ad
antonmi <anton.mishchuk@gmail.com>
2015-09-27 19:39:43 +0300

    some view specs

---
+++ test_app/spec/views/post_views_spec.exs
@@ -0,0 +1,34 @@
+defmodule TestApp.PostViewsSpec do
+  use ESpec.Phoenix, view: TestApp.PostView
+
+  alias TestApp.Post
+  let :post, do: %Post{id: 1, title: "Post title 1"}
+  let :post2, do: %Post{id: 2, title: "Post title 2"}
+  let :posts, do: [post, post2]
+
+  describe "index" do
+    subject do: render("index.html", posts: posts)
+
+    it do: should have_text("Listing posts")
+    it do: should have_text_in("table td", post.title)
+    it do: should have_text_in("table td", post2.title)
+  end
+
+  describe "show" do
+    subject do: render("show.html", post: post)
+
+    it do: should have_text("Show post")
+    it do: should have_text_in("ul li", post.title)
+    it do: should have_attribute_in("a", href: post_path(conn, :index))
+  end
+
+  describe "new" do
+    let :changeset, do: Post.changeset(%Post{})
+    subject do: render("new.html", changeset: changeset)
+
+    it do: should have_text("New post")
+    it do: should have_attributes_in("form", action: "/posts", method: "post")
+    it do: should have_selector("input#post_title")
+    it do: should have_selector("input#post_body")
+  end
+end
