9f448fbd62754939f87afaa9df781ef492c1cb8c
Anton Mishchuk <antonmi@MacBook-Pro-Anton.local>
2015-10-02 17:06:17 +0300

    new test configs

--- test_app/config/test.exs
+++ test_app/config/test.exs
@@ -15,8 +15,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :test_app, TestApp.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "antonmi",
+  password: "",
   database: "test_app_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
