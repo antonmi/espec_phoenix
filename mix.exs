5ae57d9f3639da4742fa3e79b6a44451a7c567a0
Anton Mishchuk <anton.mishchuk@gmail.com>
2016-11-25 18:51:03 +0200

    Update espec to 1.2.0

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule EspecPhoenix.Mixfile do
 
   defp deps do
     [
-      {:espec, ">= 1.1.2"},
+      {:espec, ">= 1.2.0"},
       {:phoenix, ">= 1.0.0"},
 
       {:ecto, ">= 1.0.0", only: [:dev, :test]},
