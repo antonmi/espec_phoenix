a31f733c250fa19fe0ff38de83b8824a5bf92777
Anton Mishchuk <anton.mishchuk@gmail.com>
2017-08-28 16:39:27 +0300

    Increase assert_receive timeout

--- rumbrella/apps/info_sys/test/backends/wolfram_test.exs
+++ rumbrella/apps/info_sys/test/backends/wolfram_test.exs
@@ -7,8 +7,8 @@ defmodule InfoSys.Backends.WolframTest do
     {:ok, pid} = Wolfram.start_link("1 + 1", ref, self(), 1)
     Process.monitor(pid)
 
-    assert_receive {:results, ^ref, [%InfoSys.Result{text: "2"}]}
-    assert_receive {:DOWN, _ref, :process, ^pid, :normal}
+    assert_receive {:results, ^ref, [%InfoSys.Result{text: "2"}]}, 1000
+    assert_receive {:DOWN, _ref, :process, ^pid, :normal}, 1000
   end
 
   test "no query results reports an empty list" do
