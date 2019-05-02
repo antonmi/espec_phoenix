5c4f1b74c2273d68b7f04dba0d3dbc8fdd27ec99
Jan Fornoff <janfornoff@gmail.com>
2019-05-02 15:42:43 +0200

    Allow Poison 4

--- mix.exs
+++ mix.exs
@@ -4,15 +4,17 @@ defmodule EspecPhoenix.Mixfile do
   @version "0.7.0"
 
   def project do
-    [app: :espec_phoenix,
-     name: "ESpec Phoenix",
-     version: @version,
-     elixir: ">= 1.5.3",
-     description: description(),
-     package: package(),
-     deps: deps(),
-     source_url: "https://github.com/antonmi/espec_phoenix",
-     preferred_cli_env: [espec: :test]]
+    [
+      app: :espec_phoenix,
+      name: "ESpec Phoenix",
+      version: @version,
+      elixir: ">= 1.5.3",
+      description: description(),
+      package: package(),
+      deps: deps(),
+      source_url: "https://github.com/antonmi/espec_phoenix",
+      preferred_cli_env: [espec: :test]
+    ]
   end
 
   def application do
@@ -29,17 +31,17 @@ defmodule EspecPhoenix.Mixfile do
       {:phoenix, "~> 1.4"},
       {:ecto, ">= 2.1.6", only: [:dev, :test]},
       {:ex_doc, "~> 0.16", only: :dev},
-      {:poison, "3.1.0"},
-      {:credo, "~> 0.8", only: :dev},
+      {:poison, "3.1.0 or ~> 4.0.1"},
+      {:credo, "~> 0.8", only: :dev}
     ]
   end
 
   defp package do
-   [
-     files: ~w(lib mix.exs README.md),
-     maintainers: ["Anton Mishchuk"],
-     licenses: ["MIT"],
-     links: %{"github" => "https://github.com/antonmi/espec_phoenix"}
-   ]
- end
+    [
+      files: ~w(lib mix.exs README.md),
+      maintainers: ["Anton Mishchuk"],
+      licenses: ["MIT"],
+      links: %{"github" => "https://github.com/antonmi/espec_phoenix"}
+    ]
+  end
 end
