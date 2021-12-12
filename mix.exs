7928289c25698a0b068e23f79a49391b54ca69de
Kian-Meng, Ang <kianmeng@cpan.org>
2021-12-12 12:25:02 +0800

    Misc doc changes
    Besides other documentation changes, this commit includes all relevant
    documents to the generated HTML doc for HexDocs.pm and leverages on
    latest features of ExDoc.

--- mix.exs
+++ mix.exs
@@ -1,6 +1,7 @@
 defmodule EspecPhoenix.Mixfile do
   use Mix.Project
 
+  @source_url "https://github.com/antonmi/espec_phoenix"
   @version "0.8.0"
 
   def project do
@@ -9,20 +10,18 @@ defmodule EspecPhoenix.Mixfile do
       name: "ESpec Phoenix",
       version: @version,
       elixir: ">= 1.6.0",
-      description: description(),
       package: package(),
       deps: deps(),
-      source_url: "https://github.com/antonmi/espec_phoenix",
-      preferred_cli_env: [espec: :test]
+      docs: docs(),
+      preferred_cli_env: [
+        docs: :docs,
+        espec: :test
+      ]
     ]
   end
 
   def application do
     [applications: [:logger, :phoenix]]
-  end
-
-  defp description do
-    "ESpec for Phoenix web framework."
   end
 
   defp deps do
@@ -31,7 +30,7 @@ defmodule EspecPhoenix.Mixfile do
       {:phoenix, "~> 1.6"},
       {:phoenix_live_view, "~> 0.16"},
       {:ecto, ">= 2.1.6", only: [:dev, :test]},
-      {:ex_doc, "~> 0.25", only: :dev},
+      {:ex_doc, ">= 0.0.0", only: :docs, runtime: false},
       {:poison, "~> 3.1 or ~> 4.0"},
       {:credo, "~> 1.6", only: :dev}
     ]
@@ -39,10 +38,24 @@ defmodule EspecPhoenix.Mixfile do
 
   defp package do
     [
+      description: "ESpec for Phoenix web framework.",
       files: ~w(lib mix.exs README.md),
       maintainers: ["Anton Mishchuk"],
       licenses: ["MIT"],
-      links: %{"github" => "https://github.com/antonmi/espec_phoenix"}
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [
+        "LICENSE.md": [title: "License"],
+        "README.md": [title: "Overview"]
+      ],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}",
+      formatters: ["html"]
     ]
   end
 end
